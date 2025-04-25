259218687588161b998146631f53d3362853108d
Ray <ray@rayps.com>
2025-04-26 01:29:58 +0800

    Add ssc335_lite_h3c_tc-2101

---
+++ devices/ssc335_lite_h3c_tc-2101/general/overlay/usr/share/openipc/customizer.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Perform basic settings on a known IP camera
+#
+#
+# Set custom upgrade url
+#
+fw_setenv upgrade 'https://github.com/RayPS/builder/releases/download/latest/ssc335_lite_h3c_tc-2101-nor.tgz'
+#
+#
+# Set custom majestic settings
+#
+cli -s .nightMode.irCutPin1 79
+cli -s .nightMode.irCutPin2 78
+cli -s .nightMode.backlightPin 14
+cli -s .nightMode.colorToGray true
+cli -s .image.mirror true
+cli -s .image.flip true
+cli -s .audio.speakerPin 15
+cli -s .audio.outputVolume 80
+cli -s .video0.codec h264
+#
+#
+# Set wlan device and credentials if need
+#
+fw_setenv wlandev rtl8188fu-generic
+#fw_setenv wlanssid Router
+#fw_setenv wlanpass 12345678
+
+exit 0
